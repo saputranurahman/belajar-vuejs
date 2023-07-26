1af7779c2db92839b2c9c63ac96b5e825a829dd8
unknown <saputranurahman24@gmail.com>
2023-07-26 15:37:05 +0700

    latihan baru

---
+++ JS Project/belajar-vue2/bodyComponent.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { TheCard } from 'flowbite-vue'
+</script>
+<template>
+   
+    <div class="grid grid-cols-4 gap-4">
+  <div><the-card variant="image" img-src="https://static.nike.com/a/images/t_default/abeb20f9-baf1-42b4-84c2-20c4df54621c/air-jordan-1-low-shoes-2mLM4c.png"     img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nike Air Jordan 1 Low Black, Gym red and white TITAN</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </the-card></div>
+  
+  <!-- ... -->
+  <div><the-card variant="image" img-src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//104/MTA-58765801/air_jordan_air_jordan_1_low_se_homage_split_white_black_-w-_-dr0502-101-_full01_hesfo6g9.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Air Jordan 1 Low SE Homage Split White Black</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </the-card>
+
+    </div>
+    <div>
+        <the-card variant="image" img-src="https://theluxeculture.com/wp-content/uploads/2022/08/1646122256868223.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nike Air Jordan 1 Berry <br> Pink</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </the-card>
+    </div>
+    <div>
+        <the-card variant="image" img-src="https://www.footlocker.id/media/catalog/product/cache/e81e4f913a1cad058ef66fea8e95c839/0/1/01-NIKE-FFSSBNIK5-NIKFD1437400-Blue.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nike Air Jordan Retro <br> High</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </the-card>
+    </div>
+
+</div>
+   
+
+ 
+</template>
+<style>
+.seler{
+    font-family: 'Montserrat', 'sans-serif';
+    font-size: 40px;
+    font-weight: bold;
+}
+</style>
---
+++ JS Project/belajar-vue2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<NavbarComponent/>
+
+  <HeaderComponent/>
+  <BodyComponent/>
+  <br>
+  <body2Component/>
+  <about/>
+  <br>
+  <footerComponent/>
+
+ 
+</template>
+
+<script setup>
+import NavbarComponent from '../NavbarComponent.vue';
+import HeaderComponent from '../HeaderComponent.vue'
+import BodyComponent from '../bodyComponent.vue';
+import body2Component from '../body2Component.vue'
+import about from '../about.vue'
+
+import footerComponent from '../footerComponent.vue';
+
+</script>
